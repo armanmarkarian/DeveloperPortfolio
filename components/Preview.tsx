3bd89be4bcd091fddd4ea60192e0c2ddd0c03baf
Arman <am2738@cornell.edu>
2025-07-17 14:30:18 +0200

    Fix projects

--- components/Preview.tsx
+++ components/Preview.tsx
@@ -26,55 +26,6 @@ export default function TemplatePreview({ username, name, subtext, repoLinks, te
   const [repos, setRepos] = useState<any[]>([]);
   const template = templates[templateKey];
 
-  {projects.length > 0 && (
-    <div
-      className="grid mt-6"
-      style={{
-        gap: template.layout?.gap,
-      }}
-    >
-      {projects.map((proj, i) => (
-        <div
-          key={`custom-${i}`}
-          style={{
-            backgroundColor: template.colors.cardBackground,
-            padding: template.cardStyle.padding,
-            borderRadius: template.cardStyle.borderRadius,
-            boxShadow: template.cardStyle.boxShadow,
-          }}
-        >
-          <h3 style={{ fontWeight: 700 }}>{proj.name}</h3>
-          <p>{proj.description}</p>
-          <div className="flex flex-wrap gap-2 mt-2 text-sm">
-            {proj.tags.map((tag, idx) => (
-              <span
-                key={idx}
-                className="px-2 py-1 rounded bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-100"
-              >
-                {tag}
-              </span>
-            ))}
-          </div>
-          {proj.link && (
-            <a
-              href={proj.link}
-              target="_blank"
-              rel="noreferrer"
-              style={{
-                color: template.colors.accent,
-                textDecoration: 'underline',
-                display: 'inline-block',
-                marginTop: '0.5rem',
-              }}
-            >
-              View Project
-            </a>
-          )}
-        </div>
-      ))}
-    </div>
-  )}
-
   useEffect(() => {
     async function fetchRepos() {
       const results = await Promise.all(
@@ -129,17 +80,68 @@ export default function TemplatePreview({ username, name, subtext, repoLinks, te
           {subtext}
         </p>
         {showGithubIcon && username && (
-  <a
-    href={`https://github.com/${username}`}
-    target="_blank"
-    rel="noreferrer"
-    className="inline-flex items-center mt-2 text-blue-500 hover:underline"
-  >
-    <FaGithub className="w-5 h-5 mr-1" />
-    @{username}
-  </a>
-)}
-
+          <a
+            href={`https://github.com/${username}`}
+            target="_blank"
+            rel="noreferrer"
+            className="inline-flex items-center mt-2 text-blue-500 hover:underline"
+          >
+            <FaGithub className="w-5 h-5 mr-1" />
+            @{username}
+          </a>
+        )}
+  
+        {/* Render projects if any */}
+        {projects.length > 0 && (
+          <div
+            className="grid mt-6"
+            style={{
+              gap: template.layout?.gap,
+            }}
+          >
+            {projects.map((proj, i) => (
+              <div
+                key={`custom-${i}`}
+                style={{
+                  backgroundColor: template.colors.cardBackground,
+                  padding: template.cardStyle.padding,
+                  borderRadius: template.cardStyle.borderRadius,
+                  boxShadow: template.cardStyle.boxShadow,
+                }}
+              >
+                <h3 style={{ fontWeight: 700 }}>{proj.name}</h3>
+                <p>{proj.description}</p>
+                <div className="flex flex-wrap gap-2 mt-2 text-sm">
+                  {proj.tags.map((tag, idx) => (
+                    <span
+                      key={idx}
+                      className="px-2 py-1 rounded bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-100"
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+                {proj.link && (
+                  <a
+                    href={proj.link}
+                    target="_blank"
+                    rel="noreferrer"
+                    style={{
+                      color: template.colors.accent,
+                      textDecoration: 'underline',
+                      display: 'inline-block',
+                      marginTop: '0.5rem',
+                    }}
+                  >
+                    View Project
+                  </a>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
+  
+        {/* Render repos */}
         <div
           className="grid mt-6"
           style={{
