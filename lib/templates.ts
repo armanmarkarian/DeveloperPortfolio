78a365e577c38c4f904052ffc8833f1316902279
Arman <am2738@cornell.edu>
2025-07-17 16:01:57 +0200

    More templates

--- lib/templates.ts
+++ lib/templates.ts
@@ -1,9 +1,15 @@
 import light from '../app/templates/light.json';
 import dark from '../app/templates/dark.json';
+import green from '../app/templates/green.json';
+import glass from '../app/templates/glass.json';
+import blue from '../app/templates/blue.json';
 
 export const templates = {
   light,
   dark,
+  green,
+  glass,
+  blue,
 };
 
 export type TemplateKey = keyof typeof templates;
